dfb611211bf3b8009b7a169f7a4dd7c02fd48230
Gilles <gilles.arcas@gmail.com>
2020-04-29 15:59:11 +0200

    Add import_blogger

--- fboff.py
+++ fboff.py
@@ -9,15 +9,18 @@ import glob
 import shutil
 import re
 from datetime import datetime
+from urllib.request import urlopen
 
 import ftfy
 import clipboard
 
 
 USAGE = """
-fboff.py --html --input <json fb export directory> --output <reference html directory>
-fboff.py --fixnum --input <html directory> --output <html directory>
-fboff.py --blogger --input <reference html directory> --output <blogger ready html>
+fboff.py --import_fb      --input <json fb export directory> --output <reference html directory>
+fboff.py --fixnum         --input <html directory> --output <html directory>
+fboff.py --import_blogger --input <blogger post url> --output <reference html directory>
+fboff.py --export_blogger --input <reference html directory>
+    Copy blogger ready html into clipboard
 """
 
 
@@ -30,11 +33,13 @@ MOIS = 'janvier février mars avril mai juin juillet août septembre octobre nov
 
 
 def is_title(line):
-    return 'km' in line or any(_ in line for _ in MOIS)
+    # risky. any line with a date is considered a title when in first line
+    pattern = r'(?:%s )?(1er|\d|\d\d) (%s)\b' % ('|'.join(JOURS), '|'.join(MOIS))
+    return re.search(pattern, line)
 
 
 def date_from_title(title, year):
-    pattern = r'(?:%s )?(1er|\d|\d\d) (%s)' % ('|'.join(JOURS), '|'.join(MOIS))
+    pattern = r'(?:%s )?(1er|\d|\d\d) (%s)\b' % ('|'.join(JOURS), '|'.join(MOIS))
     m = re.search(pattern, title)
     if not m:
         return None
@@ -65,11 +70,7 @@ def parse_json_photo(args, photo):
             for line in description:
                 record.append(line)
             record.append('<br />')
-
-    #destdir = os.path.join(args.output, datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d'))
-    destdir = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d')
-    new_uri = os.path.join(destdir, os.path.basename(uri))
-    record.append(IMGPAT % (new_uri, new_uri))
+    record.append(IMGPAT % (uri, uri))
     return ts, record, uri
 
 
@@ -77,7 +78,6 @@ def parse_json(args, json_export_dir):
     records = dict()
     json_dir = os.path.join(json_export_dir, 'photos_and_videos', 'album')
     for json_file in glob.glob(os.path.join(json_dir, '*.json')):
-        print('---', json_file)
         with open(json_file) as f:
             x = json.load(f)
         for photo in x['photos']:
@@ -87,23 +87,34 @@ def parse_json(args, json_export_dir):
         yield ts, record, uri
 
 
-def make_html_reference(args):
+def import_fb(args):
+    os.makedirs(args.output, exist_ok=True)
+
     with open(os.path.join(args.output, 'index.htm'), 'wt', encoding='utf-8') as f:
         print(BEGIN, file=f)
 
+        date = '2000-01-01'
+        numimg = 0
         for ts, photo, uri in parse_json(args, args.input):
             for line in photo:
-                print(line, file=f)
+                if is_title(line):
+                    date = date_from_title(line, 2019)  # TODO: paramétrer année
+                    numimg = 0
+                m = re.search(r'img src=([^ ]+)', line)  # TODO: prendre en compte img with ... src ...
+                if m:
+                    numimg += 1
+                    name = m.group(1)
+                    newname = date + '-' + str(numimg) + os.path.splitext(name)[1]
+                    shutil.copy2(os.path.join(args.input, uri), os.path.join(args.output, newname))
+                    line = IMGPAT % (newname, newname)
+                print(line.strip(), file=f)
             print(file=f)
-            destdir = os.path.join(args.output, datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d'))
-            os.makedirs(destdir, exist_ok=True)
-            shutil.copy2(os.path.join(args.input, uri), destdir)
 
         print(END, file=f)
 
 
-def html_records(args):
-    with open(os.path.join(args.input, 'index.htm'), encoding='utf-8') as f:
+def html_records(url):
+    with open(url, encoding='utf-8') as f:
         line = next(f)
         while SEP not in line:
             line = next(f)
@@ -123,7 +134,7 @@ def fix_photo_names(args):
         print(BEGIN, file=f)
         print(file=f)
 
-        for record in html_records(args):
+        for record in html_records(os.path.join(args.input, 'index.htm')):
             if is_title(record[1]):
                 date = date_from_title(record[1], 2019)  # TODO: paramétrer année
                 numimg = 0
@@ -151,17 +162,74 @@ def prepare_for_blogger(args):
     clipboard.copy(''.join(buffer))
 
 
+def import_blogger(args):
+    """
+    Import html and photos from blogger and make the simplified page.
+    """
+    with urlopen(args.input) as u:
+        buffer = u.read()
+        buffer = buffer.decode('utf-8')
+
+    tmp_name = os.path.join(args.output, 'tmp.htm')
+    with open(tmp_name, 'wt', encoding='utf-8') as f:
+        f.write(buffer)
+
+    date = '2000-01-01'
+    numimg = 0
+    with open(os.path.join(args.output, 'index.htm'), 'wt', encoding='utf-8') as f:
+        print(BEGIN, file=f)
+        print(file=f)
+        br = False
+
+        for record in html_records(tmp_name):
+            if is_title(record[1]):
+                date = date_from_title(record[1], 2019)  # TODO: paramétrer année
+                numimg = 0
+            for line in record:
+                m = re.search(r'img [^<>]*src="([^ ]+)"', line)
+                if m:
+                    numimg += 1
+                    name = m.group(1)
+                    newname = date + '-' + str(numimg) + os.path.splitext(name)[1]
+                    line = IMGPAT % (newname, newname)
+                    with urlopen(name) as u, open(os.path.join(args.output, newname), 'wb') as fimg:
+                        fimg.write(u.read())
+
+                # remove some tags
+                tags = ('div', 'table', 'tbody', 'tr')
+                pattern = '^<(' + '|'.join(tags) + '|/' + '|/'.join(tags) + ')'
+                m = re.search(pattern, line)
+                if m:
+                    continue
+
+                # remove consecutive br
+                if line == '<br />\n':
+                    if br:
+                        continue
+                    br = True
+                else:
+                    br = False
+
+                print(line.strip(), file=f)
+            print(file=f)
+
+        print(END, file=f)
+    os.remove(tmp_name)
+
+
 def parse_command_line():
     parser = argparse.ArgumentParser(description=None, usage=USAGE)
-    parser.add_argument('--html', help='input json export, output html reference',
-                        action='store', default=None)
+    parser.add_argument('--import_fb', help='input json export, output html reference',
+                        action='store_true', default=None)
     parser.add_argument('--fixnum', help='fix photo names renaming as date+index',
+                        action='store_true', default=None)
+    parser.add_argument('--export_blogger', help='input html reference, html extract blogger ready in clipboard',
+                        action='store_true', default=False)
+    parser.add_argument('--import_blogger', help='blogger post url, output html reference',
+                        action='store_true', default=False)
+    parser.add_argument('-i', '--input', help='input parameter',
                         action='store', default=None)
-    parser.add_argument('--blogger', help='input html reference, output html extract vlogger ready',
-                        action='store', default=None)
-    parser.add_argument('--input', help='input directory',
-                        action='store', default=None)
-    parser.add_argument('--output', help='output directory',
+    parser.add_argument('-o', '--output', help='output parameter',
                         action='store', default=None)
     args = parser.parse_args()
     return args
@@ -170,14 +238,17 @@ def parse_command_line():
 def main():
     args = parse_command_line()
 
-    if args.html:
-        make_html_reference(args)
+    if args.import_fb:
+        import_fb(args)
 
-    if args.fixnum:
+    elif args.fixnum:
         fix_photo_names(args)
 
-    elif args.blogger:
+    elif args.export_blogger:
         prepare_for_blogger(args)
+
+    elif args.import_blogger:
+        import_blogger(args)
 
 
 if __name__ == '__main__':
